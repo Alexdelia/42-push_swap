0beccb8541099ab0957f187b1b8c87b252da184d
Alexdelia <alexandre.delille.57@gmail.com>
2021-03-27 02:18:24 +0100

    finish my advance sorted, will test it and might improve it

--- srcs/sort_algo/ft_alex_sort.c
+++ srcs/sort_algo/ft_alex_sort.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 00:31:33 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/27 02:06:21 by adelille         ###   ########.fr       */
+/*   Updated: 2021/03/27 02:17:44 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,9 @@ int	ft_alex_sort(t_arg arg, int v)
 {
 	int	n_command;
 	int	next_big;
+	int	old_big;
 	int	next_small;
+	int old_small;
 
 	n_command = ft_separate(&arg, v);
 	next_big = ft_biggest(arg.b);
@@ -75,12 +77,30 @@ int	ft_alex_sort(t_arg arg, int v)
 		n_command += ft_need_ss(&arg, v);
 		if (ft_chek_sort(arg.b) == FALSE && ft_check_unsort(arg.b) == FALSE)
 		{
+			old_big = next_big;
 			next_big = ft_next_big(arg.b, next_big);
 			n_command += ft_opti_top(&arg.b, next_big, v, "b\n");
 			ft_op_p(&arg.b, &arg.a);
 			if (v == TRUE)
+				ft_ps("pa\n");
+			n_command++;
+			n_command += ft_opti_bot(&arg.b, old_big, v, "b\n");
+			ft_op_p(&arg.a, &arg.b);
+			if (v == TRUE)
+				ft_ps("pb\n");
+			old_small = next_small;
+			next_small = ft_next_small(arg.a, next_small);
+			n_command += ft_opti_top(&arg.a, next_small, v, "a\n");
+			ft_op_p(&arg.a, &arg.b);
+			if (v == TRUE)
 				ft_ps("pb\n");
 			n_command++;
+			n_command += ft_opti_bot(&arg.a, old_small, v, "a\n");
+			ft_op_p(&arg.b, &arg.a);
+			ft_op_rr(&arg.a);
+			ft_op_rr(&arg.b);
+			if (v == TRUE)
+				ft_ps("pa\nrrr\n");
 		}
 	}
 }
