89e683969300dd9f01de5e0435a888e0b57fb618
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-01 16:01:14 +0200

    fix and no leaks

--- srcs/sort_algo/ft_sort_3.c
+++ srcs/sort_algo/ft_sort_3.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 02:20:58 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/29 16:00:52 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/01 15:59:50 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_sort_3(t_arg *arg)
 {
-	if (arg->a->data == ft_biggest(arg.a))
+	if (arg->a->data == ft_biggest(arg->a))
 	{
-		if (ft_lst_last(arg->a) == ft_smallest(arg.a))
+		if (ft_lst_last(arg->a) == ft_smallest(arg->a))
 			return (ft_ps("sa\nrra\n"));
 		return (ft_ps("ra\n"));
 	}
-	else if (arg->a->data != ft_smallest(arg.a))
+	else if (arg->a->data != ft_smallest(arg->a))
 	{
-		if (ft_lst_last(arg->a) == ft_smallest(arg.a))
+		if (ft_lst_last(arg->a) == ft_smallest(arg->a))
 			return (ft_ps("rra\n"));
 		return (ft_ps("sa\n"));
 	}
