cbfa6887354a4376d2372b97c4a1c4559b712478
Alexdelia <alexandre.delille.57@gmail.com>
2021-05-09 14:05:16 +0200

    add mt free + all

--- srcs/markup/ft_mt_free.c
+++ srcs/markup/ft_mt_free.c
@@ -1,32 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_mt_r.c                                          :+:      :+:    :+:   */
+/*   ft_mt_free.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/03 22:50:14 by adelille          #+#    #+#             */
-/*   Updated: 2021/05/09 13:44:18 by adelille         ###   ########.fr       */
+/*   Created: 2021/05/09 13:44:27 by adelille          #+#    #+#             */
+/*   Updated: 2021/05/09 14:02:24 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
 
-void	ft_mt_r(t_markup *mt, int type)
+void	ft_mt_free(t_mt *mt)
 {
-	t_mt	*tmp;
+	if (mt)
+		free(mt);
+}
 
-	if (type == MT_A)
+void	ft_mt_freeall(t_mt **mt)
+{
+	t_mt	*mt;
+
+	if (!mt || !*mt)
+		return ;
+	while (*mt)
 	{
-		tmp = mt->a;
-		mt->a = mt->a->next;
-		ft_mt_addback(&mt->a, ft_mt_new(tmp->nb, tmp->index, tmp->keep));
-	}	
-	else if (type == MT_B)
-	{
-		tmp = mt->b;
-		mt->b = mt->b->next;
-		ft_mt_addback(&mt->b, ft_mt_new(tmp->nb, tmp->index, tmp->keep));
+		tmp = (*mt)->next;
+		ft_mt_free(*mt);
+		*mt = tmp;
 	}
-	ft_mt_free(tmp);
 }
