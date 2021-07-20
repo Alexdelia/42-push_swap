34d278ab6a6e674419f486f3d1cca59d58eb874f
Alexdelia <alexandre.delille.57@gmail.com>
2021-07-20 15:09:43 +0200

    recode move_b

--- srcs/solve/ft_solve_b.c
+++ srcs/solve/ft_solve_b.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 17:12:03 by adelille          #+#    #+#             */
-/*   Updated: 2021/07/19 17:22:45 by adelille         ###   ########.fr       */
+/*   Updated: 2021/07/20 15:09:05 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,36 @@ static t_sbs	*ft_init_sbs(void)
 	sbs->size = 0;
 	sbs->set = FALSE;
 	return (sbs);
+}
+
+static void	ft_move_b(t_markup *a, t_markup *b,
+			t_sbs *sbs, t_arg_list *arg_list)
+{
+	while (sbs->mt_a != a->head || sbs->mt_b != b->head)
+	{
+		if (sbs->dir_a == sbs->dir_b
+			&& sbs->mt_a != a->head && sbs->mt_b != b->head)
+		{
+			if (sbs->dir_a == R_UP)
+				ft_mt_rr(a, b, "rr\n", arg_list);
+			else
+				ft_mt_rrr(a, b, "rrr\n", arg_list);
+		}
+		else if (sbs->mt_a != a->head)
+		{
+			if (sbs->dir_a == R_UP)
+				ft_mt_rx(a, b, "ra\n", arg_list);
+			else
+				ft_mt_rrx(a, b, "rra\n", arg_list);
+		}
+		else if (sbs->mt_b != b->head)
+		{
+			if (sbs->dir_b == R_UP)
+				ft_mt_rx(a, b, "rb\n", arg_list);
+			else
+				ft_mt_rrx(a, b, "rrb\n", arg_list);
+		}
+	}
 }
 
 void	ft_solve_b(t_markup *a, t_markup *b, t_arg_list *arg_list)
