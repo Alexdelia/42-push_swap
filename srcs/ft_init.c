8f22d4d892fa0ba922ff08871041d05504720347
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-13 18:51:58 +0200

    fix dumb malloc

--- srcs/ft_init.c
+++ srcs/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 15:23:07 by adelille          #+#    #+#             */
-/*   Updated: 2021/07/20 15:49:10 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/13 18:51:37 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_arg_list	*ft_init_arg_list(void)
 {
 	t_arg_list	*arg_list;
 
-	arg_list = (t_arg_list *)ft_memalloc(sizeof(t_markup));
+	arg_list = (t_arg_list *)ft_memalloc(sizeof(t_arg_list));
 	if (!arg_list)
 		ft_exit("Error: Malloc failed in ft_init_arg_list\n");
 	arg_list->size = 0;
