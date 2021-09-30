f44b9134694dc0026cb8cdefa5c90fe6c3b97a84
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-30 18:28:59 +0200

    add swap and push operation

--- srcs/stack/ft_p.c
+++ srcs/stack/ft_p.c
@@ -6,10 +6,37 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 18:17:27 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/30 18:17:55 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:26:17 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
 
+void	ft_p(t_stack **s1, t_stack **s2)
+{
+	t_stack	*head;
 
+	if (!*s1)
+		return ;
+	head = *s1;
+	*s1 = head->next;
+	if (*s2)
+		head->next = (*s2);
+	else
+		head->next = NULL;
+	*s2 = head;
+}
+
+void	ft_pa(t_data *d)
+{
+	ft_p(&(d->a), &(d->b));
+	if (d->print)
+		ft_ps("pa\n");
+}
+
+void	ft_pb(t_data *d)
+{
+	ft_p(&(d->b), &(d->a));
+	if (d->print)
+		ft_ps("pb\n");
+}
--- srcs/stack/ft_s.c
+++ srcs/stack/ft_s.c
@@ -6,9 +6,44 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 18:13:45 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/30 18:18:09 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:28:43 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
 
+void	ft_s(t_stack **s)
+{
+	t_stack	*tmp;
+	t_stack	*third;
+
+	if (!*s || !(*s)->next)
+		return ;
+	third = (*s)->next->next;
+	tmp = *s;
+	*s = (*s)->next;
+	(*s)->next = tmp;
+	tmp->next = third;
+}
+
+void	ft_sa(t_data *d)
+{
+	ft_s(&(d->a));
+	if (d->print)
+		ft_ps("sa\n");
+}
+
+void	ft_sb(t_data *d)
+{
+	ft_s(&(d->b));
+	if (d->print)
+		ft_ps("sb\n");
+}
+
+void	ft_ss(t_data *d)
+{
+	ft_s(&(d->a));
+	ft_s(&(d->b));
+	if (d->print)
+		ft_ps("ss\n");
+}
