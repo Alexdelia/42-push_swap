f4e6476aac75cd1a481978fe08ad92e089f8f7fa
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-30 18:19:28 +0200

    ops

--- srcs/ft_stack_sort.c
+++ srcs/ft_stack_sort.c
@@ -6,11 +6,21 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 16:10:04 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/30 18:16:14 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:19:18 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
+
+void	ft_swap_stack(t_stack **x, t_stack *y)
+{
+	t_stack	*tmp;
+
+	tmp = *x;
+	*x = y->next_sort;
+	tmp->next_sort = y->next_sort->next_sort;
+	(*x)->next_sort = tmp;
+}
 
 void	ft_stack_sort(t_stack **head)
 {
@@ -24,9 +34,9 @@ void	ft_stack_sort(t_stack **head)
 		if (last->data - last->next_sort->data > 0)
 		{
 			if (perv == NULL)
-				ft_s(head, last);
+				ft_swap_stack(head, last);
 			else
-				ft_s(&(prev->next_sort), last);
+				ft_swap_stack(&(prev->next_sort), last);
 			last = *head;
 			prev = NULL;
 		}
