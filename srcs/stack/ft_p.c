cc3b4e5ce0bc08ba1a221ab0ba4d83bb32942f35
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-08 11:17:24 +0200

    I'm a dumbass

--- srcs/stack/ft_p.c
+++ srcs/stack/ft_p.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 18:17:27 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/30 18:56:14 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/08 11:17:05 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,14 @@ static void	ft_p(t_stack **s1, t_stack **s2)
 
 void	ft_pa(t_data *d)
 {
-	ft_p(&(d->a), &(d->b));
+	ft_p(&(d->b), &(d->a));
 	if (d->print)
 		ft_ps("pa\n");
 }
 
 void	ft_pb(t_data *d)
 {
-	ft_p(&(d->b), &(d->a));
+	ft_p(&(d->a), &(d->b));
 	if (d->print)
 		ft_ps("pb\n");
 }
