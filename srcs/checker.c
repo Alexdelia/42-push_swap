8e3ac7d4be32946332b7ce035a71c99edb2faa24
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-10 17:29:26 +0200

    recoded

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,11 +6,66 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 15:05:39 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/10 17:22:26 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/10 17:28:47 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ps.h"
+
+static int	ft_do_op(char *op, t_data *d)
+{
+	if (!ft_strcmp(name, "sa"))
+		swap_a(mem);
+	else if (!ft_strcmp(name, "sb"))
+		swap_b(mem);
+	else if (!ft_strcmp(name, "ss"))
+		swap_b_a(mem);
+	else if (!ft_strcmp(name, "pa"))
+		push_a(mem);
+	else if (!ft_strcmp(name, "pb"))
+		push_b(mem);
+	else if (!ft_strcmp(name, "ra"))
+		rotate_a(mem);
+	else if (!ft_strcmp(name, "rb"))
+		rotate_b(mem);
+	else if (!ft_strcmp(name, "rr"))
+		rotate_b_a(mem);
+	else if (!ft_strcmp(name, "rra"))
+		rev_rotate_a(mem);
+	else if (!ft_strcmp(name, "rrb"))
+		rev_rotate_b(mem);
+	else if (!ft_strcmp(name, "rrr"))
+		rev_rotate_b_a(mem);
+	else
+		return (FALSE);
+	return (TRUE);
+}
+
+static int	ft_read_operation(t_data *d)
+{
+	char	op[4];
+	int		i;
+	int		p;
+
+	 i = 0;
+	 while (i < 4)
+	 {
+		 p = i;
+		 i += read(0, op + i, 1);
+		 if (p == i)
+			break ;
+		 if (op[p] == '\n')
+		 {
+			 op[p] = 0;
+			 if (ft_do_op(op, d) == FALSE)
+				 return (FALSE);
+			 i = 0;
+		 }
+	 }
+	 if (i == 4)
+		 return (FALSE);
+	 return (TRUE);
+}
 
 int	main(int ac, char **av)
 {
@@ -22,6 +77,8 @@ int	main(int ac, char **av)
 	/*if (v == TRUE)
 		ft_psc("\nParsing ...\t", "\033[1;29m");*/
 	if (ft_arg(ac, av, &d) == FALSE)
+		return (ft_free_data(&d));
+	if (ft_read_operation(&d) == FALSE)
 		return (ft_free_data(&d));
 	/*if (v == TRUE)
 	{
