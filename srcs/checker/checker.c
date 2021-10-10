1e2ea640fc654327cd529335a790d01cf3a90fec
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-10 17:22:57 +0200

    start recoding checker with new struct and knowledge

--- srcs/checker/checker.c
+++ srcs/checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 15:05:39 by adelille          #+#    #+#             */
-/*   Updated: 2021/03/28 14:48:12 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/10 17:22:26 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 int	main(int ac, char **av)
 {
-	t_arg	arg;
-	int		v;
+	t_data	*d;
 
-	v = ft_visual(av);
-	if (v == TRUE)
-		ft_psc("\nParsing ...\t", "\033[1;29m");
-	ft_arg(&arg, ac, av);
-	if (arg.err == TRUE)
-		return (0);
-	if (v == TRUE)
+	d.a = NULL;
+	d.b = NULL;
+	d.print = 0;
+	/*if (v == TRUE)
+		ft_psc("\nParsing ...\t", "\033[1;29m");*/
+	if (ft_arg(ac, av, &d) == FALSE)
+		return (ft_free_data(&d));
+	/*if (v == TRUE)
 	{
 		ft_psc("Done\n", GRN);
 		ft_psc("\nPlease enter commands:\n", "\033[1;29m");
-	}
-	ft_checking(&arg);
-	ft_lstclear(&arg.a);
-	ft_lstclear(&arg.b);
+	}*/
+	if (ft_is_sort(&d) == TRUE)
+		ft_ps("OK");
+	else
+		ft_ps("KO");
+	ft_free_data(&d);
 	return (0);
 }
