00c17a128dff5eab27e86c9e14df6a29e917b9fe
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-12 01:12:43 +0200

    make sure .sh run in bash

--- tester.sh
+++ tester.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo 'ARG=`ruby -e "puts (1..50).to_a.shuffle.join(' ')"`; ./push_swap $ARG | ./checker $ARG'
 ARG=`ruby -e "puts (1..50).to_a.shuffle.join(' ')"`; ./push_swap $ARG | ./checker $ARG
